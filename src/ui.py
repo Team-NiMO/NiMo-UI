976001a40878d757dc428f7cea5c76dc19faaf9d
Sruthi Mukkamala <sruthim@andrew.cmu.edu>
2024-09-15 14:30:24 -0700

    Added primary functionalities for buttons to ui.py.

--- src/ui.py
+++ src/ui.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 import rospkg
 from PyQt5 import QtWidgets, uic
 
@@ -11,6 +12,10 @@ class Ui(QtWidgets.QMainWindow):
 
         # Connect Buttons to Functions
         self.exitButton.clicked.connect(self.exitEvent)
+        self.addButton.clicked.connect(self.addButtonAction)
+        self.startButton.clicked.connect(self.startButtonAction)
+        self.uploadCSVButton.clicked.connect(self.uploadCSVButtonAction)
+        self.saveCSVButton.clicked.connect(self.saveCSVButtonAction)
 
         # Setup table
         self.horizontalHeader = self.table.horizontalHeader()
@@ -18,8 +23,106 @@ class Ui(QtWidgets.QMainWindow):
         self.horizontalHeader.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
         self.horizontalHeader.setSectionResizeMode(3, QtWidgets.QHeaderView.Stretch)
 
+        self.coordinates = []
+
     def exitEvent(self):
         exit()
+
+    def deleteTableRowAction(self, row):
+        self.table.removeRow(row)
+        self.coordinates.pop(row)
+        for idx in range(self.table.rowCount()):
+            self.table.setItem(idx, 0, QtWidgets.QTableWidgetItem(str(idx+1)))
+        self.statusImage.setText("Removed coordinate.")
+    
+    def addButtonAction(self):
+        lat = self.lattidudeEdit.text()
+        long = self.longitudeEdit.text()
+
+        try:
+            self.coordinates.append([float(lat), float(long)])
+            self.statusImage.setText("Valid coordinate input.")
+        except:
+            self.statusImage.setText("Invalid coordinate input.")
+            self.lattidudeEdit.clear()
+            self.longitudeEdit.clear()
+            return
+    
+        row = self.table.rowCount()
+        self.table.insertRow(row)
+        self.table.setItem(row, 0, QtWidgets.QTableWidgetItem(str(row+1)))
+        self.table.setItem(row, 1, QtWidgets.QTableWidgetItem(lat))
+        self.table.setItem(row, 2, QtWidgets.QTableWidgetItem(long))
+        self.table.setItem(row, 3, QtWidgets.QTableWidgetItem(str("")))
+
+        btn = QtWidgets.QPushButton(self.table)
+        btn.setText("X")
+        btn.setStyleSheet("background-color:red")
+        self.table.setCellWidget(row, 4, btn)
+        self.table.cellWidget(row, 4).clicked.connect(lambda: self.deleteTableRowAction(row))
+
+        self.lattidudeEdit.clear()
+        self.longitudeEdit.clear()
+
+    def startButtonAction(self):
+        self.addButton.setDisabled(True)
+        self.uploadCSVButton.setDisabled(True)
+        self.saveCSVButton.setDisabled(False)
+        
+        row = self.table.rowCount()
+        for idx in range(row):
+            self.table.cellWidget(idx,4).setDisabled(True)
+
+        self.statusImage.setText("Robot in motion.")
+        self.startButton.setDisabled(True)
+
+    def uploadCSVButtonAction(self):
+        fileName, ok = QtWidgets.QFileDialog.getOpenFileName(self, 'Select a CSV file:', 'C:\\', "CSV (*.csv)")
+        if ok:
+            validInputs = True
+            with open(fileName, newline='') as csvfile:
+                reader = csv.reader(csvfile, delimiter=',')
+                next(reader, None) # Assume that there is a header for "Latitude, Longitude"
+                for row in reader:
+                    if len(row) != 2:
+                        validInputs = False
+                        continue
+
+                    try:
+                        self.coordinates.append([float(row[0]), float(row[1])])
+                    except:
+                        validInputs = False
+                        continue
+                    
+                    rowNum = self.table.rowCount()
+                    self.table.insertRow(rowNum)
+                    self.table.setItem(rowNum, 0, QtWidgets.QTableWidgetItem(str(rowNum+1)))
+                    self.table.setItem(rowNum, 1, QtWidgets.QTableWidgetItem(str(row[0])))
+                    self.table.setItem(rowNum, 2, QtWidgets.QTableWidgetItem(str(row[1])))
+                    self.table.setItem(rowNum, 3, QtWidgets.QTableWidgetItem(str("")))
+
+                    btn = QtWidgets.QPushButton(self.table)
+                    btn.setText("X")
+                    btn.setStyleSheet("background-color:red")
+                    self.table.setCellWidget(rowNum, 4, btn)
+            
+            if not validInputs:
+                self.statusImage.setText("Some invalid values were skipped.")
+            else:
+                self.statusImage.setText("Successful upload of .csv.")
+
+    def saveCSVButtonAction(self):
+        path, ok = QtWidgets.QFileDialog.getSaveFileName(self, 'Select a CSV file to save data:', 'C:\\', "CSV (*.csv)")
+        if ok:
+            columns = range(self.table.columnCount()-1)
+            header = [self.table.horizontalHeaderItem(column).text() for column in columns]
+            with open(path, 'w') as csvfile:
+                writer = csv.writer(csvfile, delimiter = ",")
+                writer.writerow(header)
+                for row in range(self.table.rowCount()):
+                    writer.writerow(self.table.item(row, column).text() for column in columns)
+
+        self.statusImage.setText("Data save to .csv complete.")
 
 if __name__ == "__main__":
     rospack = rospkg.RosPack()
